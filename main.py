61d4472e4c2c4d013a920b7e7db43ff8e7c9fbf2
Pramod <pramod4342@gmail.com>
2026-07-12 11:15:41 +0530

    refactor: organize environment variable setup and prompt template

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ from dotenv import load_dotenv
 import streamlit as st
 
 load_dotenv()
+
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+
+##prompt templete
+
+prompt = PromptTemplate.from_template(
+    (
+        "System",
+        "You are the helpful ai assistant . Please respond to the user question",
+    ),
+    ("user", "{question}"),
+)
