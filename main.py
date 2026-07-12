5592993ba29dbea28ddea68d852ad971cdf6fd0a
Pramod <pramod4342@gmail.com>
2026-07-12 10:58:21 +0530

    project chatBot initialization

---
+++ main.py
@@ -0,0 +1,8 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
